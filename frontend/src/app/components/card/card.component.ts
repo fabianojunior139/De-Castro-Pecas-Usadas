4826383eab217a18f0340cec8ddafafcdf486def
Fabiano Junior <fabianojunior139@gmail.com>
2023-07-12 15:29:52 -0300

    adicionando footer a página home

--- frontend/src/app/components/card/card.component.ts
+++ frontend/src/app/components/card/card.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import { faCalendar } from '@fortawesome/free-solid-svg-icons';
+import { faGasPump } from '@fortawesome/free-solid-svg-icons';
+import { faWrench } from '@fortawesome/free-solid-svg-icons';
+import { faClock } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-card',
@@ -6,5 +10,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
-
+  faCalendar = faCalendar;
+  faGasPump = faGasPump;
+  faClock = faClock;
+  faWrench = faWrench;
 }
